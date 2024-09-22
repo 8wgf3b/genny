998410e2a1da60455943288b338775014458bbb5
8wgf3b <rohith.pervala@gmail.com>
2024-09-22 14:21:43 +0530

    Default input path option

--- necromus/src/main.rs
+++ necromus/src/main.rs
@@ -5,7 +5,7 @@ use clap::Parser;
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
-    input: PathBuf,
+    input: Option<PathBuf>,
 
     #[arg(short, long, action = clap::ArgAction::Count, value_parser = clap::value_parser!(u8).range(0..3))]
     part: u8,
@@ -21,7 +21,8 @@ fn part2(_contents: &str) {
 
 fn main() {
     let cli = Cli::parse();
-    let contents = read_to_string(&cli.input).expect("Failed to read the file");
+    let p = cli.input.unwrap_or(PathBuf::from(".\\inputs\\final.txt"));
+    let contents = read_to_string(&p).expect("Failed to read the file");
     match cli.part {
         1 => part1(&contents),
         2 => part2(&contents),
